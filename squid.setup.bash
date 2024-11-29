db1feb679c6f3f684c6fdfb15b7310c85c4be4c0
patrickalantaylor <patrick@linront.com>
2024-11-29 13:37:11 -0800

    Update squid.setup.bash
    run instructions

--- squid.setup.bash
+++ squid.setup.bash
@@ -1,7 +1,9 @@
 #!/bin/bash 
+
 ## this is a script for the installation and configuraiton of the squid proxy
 ## this script was taken from https://coderevolution.ro/knowledge-base/faq/how-to-set-up-your-own-free-proxy-server/ on Nov 29 2024
 ## this script is tested on Ubuntu v 24.04 (LTS) on digital ocean droplet.
+## run this with bash ./squid.setup.bash
 
 # get apt set up
 sudo apt update -y
