0622d5aee15f27028cb51968fbd9d385ab5ca196
patrickalantaylor <patrick@linront.com>
2024-11-29 13:28:39 -0800

    Create squid.setup.bash
    first default install steps

---
+++ squid.setup.bash
@@ -0,0 +1,16 @@
+## this is a script for the installation and configuraiton of the squid proxy
+## this script was taken from https://coderevolution.ro/knowledge-base/faq/how-to-set-up-your-own-free-proxy-server/ on Nov 29 2024
+## this script is tested on Ubuntu v 24.04 (LTS) on digital ocean droplet.
+
+# get apt set up
+sudo apt update -y
+
+#apt install squid proxy
+sudo apt -y install squid
+
+#start squid
+sudo systemctl start squid
+sudo systemctl enable squid
+
+# show squid status
+sudo systemctl status squid
