c54ed413aec1a320dbb980b60280a819e55a0b60
patrickalantaylor <patrick@linront.com>
2024-11-29 13:35:12 -0800

    Update squid.setup.bash
    shabang

--- squid.setup.bash
+++ squid.setup.bash
@@ -1,3 +1,4 @@
+#!/bin/bash 
 ## this is a script for the installation and configuraiton of the squid proxy
 ## this script was taken from https://coderevolution.ro/knowledge-base/faq/how-to-set-up-your-own-free-proxy-server/ on Nov 29 2024
 ## this script is tested on Ubuntu v 24.04 (LTS) on digital ocean droplet.
